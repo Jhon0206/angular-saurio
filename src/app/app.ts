e51251cad87cc386af0fc74ef770329932ed323d
Jhon <jhon_0206@hotmail.com>
2025-11-27 20:31:50 -0500

    feat: Initialize root application component, configure schematics, and add GitHub Pages deployment.

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-saurio');
+  protected readonly title = signal('Angular Saurio');
 }
